aa8a689359acc297d53b97c54b3569e2fb8ce3fd
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-04-25 16:19:23 +0200

    Exercici 3 - Vistes i navegació

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom"
+
+
 const Header = () => {
-  return (
-    <p class="text-white text-center">Alumno: ...</p>
-  )
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <Link className="nav-link" to="/">Usuarios</Link>
+                            <Link className="nav-link" to="/camareros">Camareros</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Header
