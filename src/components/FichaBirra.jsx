b7a38fe9ec22bccf1479451bfdf81d53ed20c1a5
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-04-25 16:30:31 +0200

    Creados los componentes de NuevoPedido y FichaBirra

---
+++ src/components/FichaBirra.jsx
@@ -0,0 +1,17 @@
+const fichaBirra = ( birra ) =>{
+    return(
+        <div className="col-6 border ">
+        <div className="p-3 d-flex">
+            <div className="w-50">
+                <img src="estrella.jpg" alt="" className="w-100" />
+            </div>
+            <div>
+                <h4 className="">Estrella Galicia</h4>
+                <p>Cerveza suave y equilibrada con un sabor ligeramente amargo y aroma a malta.</p>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default fichaBirra;
